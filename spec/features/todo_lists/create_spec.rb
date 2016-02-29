04b75e8aadb9dac83d5d8d044341361b861d16cd
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2016-02-29 15:12:59 -0500

    Create todo list edit spec

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -13,8 +13,8 @@ describe "Creating todo lists" do
     fill_in "Title", with: options[:title]
     fill_in "Description", with: options[:description]
     click_button "Create Todo list"
-
   end
+  
 
   it "redirects to the todo list index page on success" do 
     create_todo_list
